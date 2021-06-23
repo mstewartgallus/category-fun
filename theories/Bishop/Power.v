ff2b76b0891be342976163d9c98e9c3fc4b63d6d
Molly Stewart-Gallus <stevenselectronicmail@gmail.com>
2021-06-23 09:07:45 -0700

    Start on powersets

---
+++ theories/Bishop/Power.v
@@ -0,0 +1,35 @@
+Require Import Blech.Defaults.
+
+Require Import Coq.Setoids.Setoid.
+Require Import Coq.Classes.SetoidClass.
+
+Require Import Blech.Bishop.
+Require Import Blech.Proset.
+Require Import Blech.Proset.Sub.
+Require Import Blech.Category.Over.
+Require Import Blech.Category.Bsh.
+
+Import BishopNotations.
+Import OverNotations.
+
+Definition Power (S: Bishop): Bishop := Sub Bsh S /~ Proset_Setoid _.
+
+#[program]
+Definition pred {S: Bishop} (p: S → Prop):Power S :=
+  lub ({ x | p x }/~ {| equiv x y := proj1_sig x == y |}),
+  λ x, proj1_sig x.
+
+Next Obligation.
+Proof.
+  admit.
+Admitted.
+
+Next Obligation.
+Proof.
+  intros ? ? ?.
+  auto.
+Qed.
+
+#[program]
+Definition fiber {S: Bishop} (p:Power S) (y: S): Prop :=
+  ∃ x, y == π p x.
