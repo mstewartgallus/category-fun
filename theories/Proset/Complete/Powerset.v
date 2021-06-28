e3f76e88cc317ede6f04c8deeb3bc842488b699d
Molly Stewart-Gallus <stevenselectronicmail@gmail.com>
2021-06-28 15:17:23 -0700

    Add yoneda-like stuff for powersets

--- theories/Proset/Complete/Powerset.v
+++ theories/Proset/Complete/Powerset.v
@@ -29,132 +29,67 @@ Open Scope bishop_scope.
 
 (* The powerset can be considered the free CABA *)
 #[program]
-Definition Powerset (S: Bishop): Complete := {|
-  P := Sub Bsh S ;
+ Definition Powerset (S: Proset): Complete := {|
+  P :=
+    {|
+      T := S → Prop ;
+      preorder p q := ∀ x, q x → p x ;
+    |}
 |}.
 
 Next Obligation.
 Proof.
-  admit.
-Admitted.
+  exists.
+  - intros ? ? t.
+    apply t.
+  - intros ? ? ? p q ? ?.
+    apply p.
+    apply q.
+    assumption.
+Qed.
 
 Next Obligation.
 Proof.
-  admit.
-Admitted.
-(* #[local] *)
-(* Definition powerset: Bishop → Proset := Sub Bsh. *)
+  destruct X.
+  cbn in *.
+  apply (∀ a, x a X0).
+Qed.
 
-(* #[local] *)
-(* #[program] *)
-(* Definition sup {S D} (f: PreOrd (D ᵒᵖ) (powerset S)): powerset S *)
-(*   := lub ((Σ x, s (f x)) /~ {| equiv x y := head x == head y |}), *)
-(*      λ '⟨ x, y ⟩, π (f x) y. *)
+Next Obligation.
+Proof.
+  destruct X.
+  cbn in *.
+  apply (∀ a, x a X0).
+Qed.
 
-(* Next Obligation. *)
-(* Proof. *)
-(*   admit. *)
-(* Admitted. *)
+(* Doesn't seem right to me ? *)
+#[program]
+Definition Yo (S: Proset): PreOrd (S ᵒᵖ) (Powerset S) := λ x y, y ⊑ x.
 
-(* Next Obligation. *)
-(* Proof. *)
-(*   intros ? ? p. *)
-(*   cbn in *. *)
-(*   destruct x, y. *)
-(*   destruct p as [p q]. *)
-(*   cbn in *. *)
-  
-(*   apply ( *)
-(*   apply (tail y). *)
-(*   cbn in *. *)
-(*   eapply x. *)
-(*   Unshelve. *)
-(*   2: { *)
-(*     cbn. *)
-(*   admit. *)
-(* Admitted. *)
+Next Obligation.
+Proof.
+  intros ? ? p ? q.
+  cbn in *.
+  rewrite q.
+  apply p.
+Qed.
 
-(* Next Obligation. *)
-(* Proof. *)
-(*   admit. *)
-(* Admitted. *)
+#[program]
+ Definition Spec S: PreOrd (Powerset (S ᵒᵖ)) (Powerset S) := λ p x, p x.
 
+Next Obligation.
+Proof.
+  intros ? ? p ? x.
+  cbn in *.
+  apply (p _ x).
+Defined.
 
+#[program]
+ Definition Cospec S: PreOrd (Powerset S) (Powerset (S ᵒᵖ)) := λ p x, p x.
 
-(* (* FIXME make a functor *) *)
-(* #[program] *)
-(*  Definition preimage {A B: Bishop} (f: Bsh B A): Powerset A ~> Powerset B := *)
-(*   λ s, lub ({ a | ∃ x, f a == proj1_sig (π s) x } /~ {| equiv x y := proj1_sig x == y |}), *)
-(*      λ y, proj1_sig y. *)
-
-(* Next Obligation. *)
-(* Proof. *)
-(*   exists. *)
-(*   - intros ?. *)
-(*     reflexivity. *)
-(*   - intros ? ? ?. *)
-(*     symmetry. *)
-(*     assumption. *)
-(*   - intros ? ? ? p q. *)
-(*     rewrite p, q. *)
-(*     reflexivity. *)
-(* Qed. *)
-
-(* Next Obligation. *)
-(* Proof. *)
-(*   intros ? ? p. *)
-(*   assumption. *)
-(* Defined. *)
-
-(* Next Obligation. *)
-(* Proof. *)
-(*   intros x y p. *)
-(*   destruct x as [sx px], y as [sy py]. *)
-(*   destruct px as [px], py as [py]. *)
-(*   cbn in *. *)
-(*   destruct p. *)
-(*   exists. *)
-(*   eexists. *)
-(*   Unshelve. *)
-(*   2: { *)
-(*     cbn in *. *)
-(*     eexists. *)
-(*     Unshelve. *)
-(*     2: { *)
-(*       cbn in *. *)
-(*       eexists. *)
-(*       Unshelve. *)
-(*       2: { *)
-(*         cbn in *. *)
-(*         intro x. *)
-(*         exists (proj1_sig x). *)
-(*         destruct X. *)
-(*         destruct x0. *)
-(*         destruct x. *)
-(*         cbn in *. *)
-(*         destruct e3. *)
-(*         exists (proj1_sig x0 x1). *)
-(*         rewrite H. *)
-(*         rewrite e1. *)
-(*         reflexivity. *)
-(*       } *)
-(*       cbn in *. *)
-(*       intros ? ? p. *)
-(*       cbn in *. *)
-(*       assumption. *)
-(*     } *)
-(*     cbn in *. *)
-(*     intros ? ? ? p ?. *)
-(*     apply p. *)
-(*   } *)
-(*   cbn in *. *)
-(*   intros ?. *)
-(*   reflexivity. *)
-(* Qed. *)
-
-(* Next Obligation. *)
-(* Proof. *)
-(*   exists. *)
-(*   - admit. *)
-(*   - admit. *)
-(* Admitted. *)
+Next Obligation.
+Proof.
+  intros ? ? p ? x.
+  cbn in *.
+  apply (p _ x).
+Defined.
