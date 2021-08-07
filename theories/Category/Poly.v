1a7772b9d9b97019a186caafb8f9b31d28d2c7d8
Molly Stewart-Gallus <stevenselectronicmail@gmail.com>
2021-08-07 14:47:15 -0700

    Write the extension of a polynomial endofunctor

--- theories/Category/Poly.v
+++ theories/Category/Poly.v
@@ -5,11 +5,16 @@ Require Import Coq.Classes.SetoidClass.
 
 Require Import Blech.Bishop.
 Require Import Blech.Category.
+Require Import Blech.Category.Prod.
+Require Import Blech.Category.Typ.
+Require Import Blech.Category.Funct.
 Require Import Blech.Functor.
+Require Import Blech.Functor.Curry.
 Require Blech.Reflect.
 
 Import CategoryNotations.
 Import FunctorNotations.
+Import ProdNotations.
 Import BishopNotations.
 
 Open Scope category_scope.
@@ -33,7 +38,6 @@ Record Cofunctor (p q: Arena) := {
 Arguments Mor_pos {p q}.
 Arguments Mor_dir {p q} c {i}.
 
-(* FIXME Poly should actually be a bicategory ? *)
 #[program]
 Definition Poly: Category := {|
   Obj := Arena ;
@@ -51,3 +55,35 @@ Definition Poly: Category := {|
       Mor_dir i x := Mor_dir g (Mor_dir f x) ;
     |} ;
 |}.
+
+Record extension (p: Poly) (x: Type): Type := ext {
+  s: pos p ;
+  π: dir p s → x ;
+}.
+
+Arguments ext {p x}.
+Arguments s {p x}.
+Arguments π {p x}.
+
+#[local]
+Definition ext_rmap {p A B} (f: A → B) (x:extension p A): extension p B :=
+  ext _ (λ y, f (π x y)).
+
+#[local]
+Definition ext_lmap {p q A} (f: Cofunctor p q) (x:extension p A): extension q A :=
+  ext (Mor_pos f (s x)) (λ y, π x (Mor_dir f y)).
+
+#[program]
+Definition Extension: Functor Poly (Funct Typ Typ) := curry {|
+  op (px: Poly * Typ) := extension (fst px) (snd px) : Typ ;
+  map '(p, A) '(q, B) '(F, G) x := ext_rmap G (ext_lmap F x) ;
+|}.
+
+Next Obligation.
+Proof.
+  intros [? ?] [? ?] [p q].
+  unfold ext_lmap, ext_rmap in *.
+  cbn in *.
+  subst.
+  reflexivity.
+Qed.
